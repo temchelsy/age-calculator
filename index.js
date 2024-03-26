e7306dd0e63f4b429b2c501dfd978e8e175e32fe
tem chelsy <temchelsy@gmail.com>
2024-03-26 10:56:24 +0100

    my age calculator

--- index.js
+++ index.js
@@ -4,55 +4,55 @@ function getDOB() {
  
     // Getting input from html input element
     let data =
-        document.getElementById("inputDob").value;
+        document.getElementById("inputDob").value
  
     // Convert input data to usable format
     // as day,month and year
-    let dob = new Date(data);
-    let day = dob.getDate();
-    let month = dob.getMonth();
-    let year = dob.getFullYear();
+    let dob = new Date(data)
+    let day = dob.getDate()
+    let month = dob.getMonth()
+    let year = dob.getFullYear()
  
     // Getting current date and calculating the difference
     let now =
-        new Date(document.getElementById("cdate").value);
+        new Date(document.getElementById("cdate").value)
     console.log(now);
-    let yearDiff = now.getFullYear() - year;
-    let monthDiff = now.getMonth() - month;
-    let dateDiff = now.getDate() - day;
+    let yearDiff = now.getFullYear() - year
+    let monthDiff = now.getMonth() - month
+    let dateDiff = now.getDate() - day
  
     // Calculating the Age
-    if (yearDiff < 0) console.log("invalid date");
+    if (yearDiff < 0) console.log("invalid date")
     else if (monthDiff > 0) {
-        console.log(yearDiff);
+        console.log(yearDiff)
     } else if (monthDiff === 0 && dateDiff >= 0) {
-        console.log(yearDiff);
+        console.log(yearDiff)
     } else {
-        yearDiff = yearDiff - 1;
+        yearDiff = yearDiff - 1
         if (monthDiff <= 0)
-            if (dateDiff > 0) monthDiff = 12 + monthDiff;
-            else monthDiff = 11 - monthDiff;
+            if (dateDiff > 0) monthDiff = 12 + monthDiff
+            else monthDiff = 11 - monthDiff
     }
     if (dateDiff < 0) {
-        dateDiff = 30 + dateDiff;
-        monthDiff -= 1;
+        dateDiff = 30 + dateDiff
+        monthDiff -= 1
     }
  
     // Show calculated age as output
     // and invalid if wrong input is given
     if (yearDiff < 0)
-        document.getElementById("currentAge").innerHTML = "Invalid Date";
+        document.getElementById("currentAge").innerHTML = "Invalid Date"
     else
         document.getElementById("currentAge").innerHTML =
             "Your current Age is " + yearDiff + " years "
-            + monthDiff + " months " + dateDiff + " days";
+            + monthDiff + " months " + dateDiff + " days"
 }
  
 // Function to provide default date value
 function currentDate() {
-    console.log(formatted());
-    let d = document.getElementById("cdate");
-    d.value = formatted();
+    console.log(formatted())
+    let d = document.getElementById("cdate")
+    d.value = formatted()
 }
  
 function formatted(date = new Date()) {
@@ -60,11 +60,11 @@ function formatted(date = new Date()) {
         date.getFullYear(),
         short(date.getMonth() + 1),
         short(date.getDate()),
-    ].join("-");
+    ].join("-")
 }
 function short(num) {
-    return num.toString().padStart(2, "0");
+    return num.toString().padStart(2, "0")
 }
  
 // Calling current date function to set default date value
-currentDate();
+currentDate()
